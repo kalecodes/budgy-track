6314f1d11e97792ad34f47fef648a195c23ae4e3
kalen510 <kalen.wiley@gmail.com>
2022-04-16 14:45:22 -0400

    service worker - deleted old cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,3 +14,24 @@ self.addEventListener('install', function (e) {
         })
     )
 });
+
+// delete outdated cache
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    )
+});
